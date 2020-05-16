a443f1c1e79c889e857334e0521ae2c2424155cd
Ivan <63844098+n1cejkee91@users.noreply.github.com>
2020-05-16 12:39:59 +0300

    second stage

--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,1 +1,18 @@
-# write your code here
+'''cells = ' '.join(input("Enter cells: ")).split(' ')
+print(cells)
+
+print("---------")
+print("| {} {} {} |".format(cells[0], cells[1], cells[2]))
+print("| {} {} {} |".format(cells[3], cells[4], cells[5]))
+print("| {} {} {} |".format(cells[6], cells[7], cells[8]))
+print("---------")'''
+
+
+
+cells = list(input("Enter cells: "))
+
+border = '-'
+print(border * 9)
+for i in range(0, 9, 3):
+    print('|', *cells[i:i + 3], '|')
+print(border * 9)
