646ae0b027012b5910d916eb023eb05ec5ddabfd
Ivan <63844098+n1cejkee91@users.noreply.github.com>
2020-05-22 17:57:26 +0300

    update

---
+++ Problems/Chaos/task.py
@@ -0,0 +1,3 @@
+print(45/9 + 16*(5 + 8))
+print("mathematics")
+
--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,14 +1,4 @@
-'''cells = ' '.join(input("Enter cells: ")).split(' ')
-print(cells)
-
-print("---------")
-print("| {} {} {} |".format(cells[0], cells[1], cells[2]))
-print("| {} {} {} |".format(cells[3], cells[4], cells[5]))
-print("| {} {} {} |".format(cells[6], cells[7], cells[8]))
-print("---------")'''
-
-cells = list(input("Enter cells: "))
-
+cells = ['_' for i in range(9)]
 
 def table(cells):
     border = '-'
@@ -17,139 +7,290 @@ def table(cells):
         print('|', *cells[i:i + 3], '|')
     print(border * 9)
 
-
 def game(cells):
-    coordinates = input('Enter the coordinates:').split()
-    if coordinates[0] in '0123456789' and coordinates[1] in '0123456789':
-        move(int(coordinates[0]), int(coordinates[1]))
+    if "_" in cells:
+        coordinates = input('Enter the coordinates:').split()
+        if coordinates[0] in '0123456789' and coordinates[1] in '0123456789':
+            if 'X' not in cells and 'O' not in cells:
+                moveX(int(coordinates[0]), int(coordinates[1]))
+            elif cells.count('X') > cells.count('O'):
+                moveO(int(coordinates[0]), int(coordinates[1]))
+            else:
+                moveX(int(coordinates[0]), int(coordinates[1]))
+        else:
+            print("You should enter numbers!")
+            return game(cells)
+
+def exit():
+    pass
+
+def wins1(cells):
+    if cells[0] == cells[1] == cells[2] != '_':
+        print(cells[0], 'wins')
+        return exit()
+    elif cells[3] == cells[4] == cells[5] != '_':
+        print(cells[3], 'wins')
+        return exit()
+    elif cells[6] == cells[7] == cells[8] != '_':
+        print(cells[6], 'wins')
+        return exit()
+    elif cells[0] == cells[3] == cells[6] != '_':
+        print(cells[0], 'wins')
+        return exit()
+    elif cells[1] == cells[4] == cells[7] != '_':
+        print(cells[1], 'wins')
+        return exit()
+    elif cells[2] == cells[5] == cells[8] != '_':
+        print(cells[2], 'wins')
+        return exit()
+    elif cells[0] == cells[4] == cells[8] != '_':
+        print(cells[0], 'wins')
+        return exit()
+    elif cells[2] == cells[4] == cells[6] != '_':
+        print(cells[2], 'wins')
+        return exit()
     else:
-        print("You should enter numbers!")
         return game(cells)
 
+def wins2(cells):
+    if cells[0] == cells[1] == cells[2]:
+        print(cells[0], 'wins')
+        return exit()
+    elif cells[3] == cells[4] == cells[5]:
+        print(cells[3], 'wins')
+        return exit()
+    elif cells[6] == cells[7] == cells[8]:
+        print(cells[6], 'wins')
+        return exit()
+    elif cells[0] == cells[3] == cells[6]:
+        print(cells[0], 'wins')
+        return exit()
+    elif cells[1] == cells[4] == cells[7]:
+        print(cells[1], 'wins')
+        return exit()
+    elif cells[2] == cells[5] == cells[8]:
+        print(cells[2], 'wins')
+        return exit()
+    elif cells[0] == cells[4] == cells[8]:
+        print(cells[0], 'wins')
+        return exit()
+    elif cells[2] == cells[4] == cells[6]:
+        print(cells[2], 'wins')
+        return exit()
+    else:
+        print('Draw')
+        return exit()
 
-def move(x, y):
+def moveX(x, y):
     if 1 <= x <= 3 and 1 <= y <= 3:
         if x == 1 and y == 1:
             if cells[6] == '_':
                 cells[6] = 'X'
-                return table(cells)
+                table(cells)
+                if '_' in cells:
+                    return wins1(cells)
+                else:
+                    return wins2(cells)
             else:
                 print("This cell is occupied! Choose another one!")
                 return game(cells)
         elif x == 1 and y == 2:
             if cells[3] == '_':
                 cells[3] = 'X'
-                return table(cells)
+                table(cells)
+                if '_' in cells:
+                    return wins1(cells)
+                else:
+                    return wins2(cells)
             else:
                 print("This cell is occupied! Choose another one!")
                 return game(cells)
         elif x == 1 and y == 3:
             if cells[0] == '_':
                 cells[0] = 'X'
-                return table(cells)
+                table(cells)
+                if '_' in cells:
+                    return wins1(cells)
+                else:
+                    return wins2(cells)
             else:
                 print("This cell is occupied! Choose another one!")
                 return game(cells)
         elif x == 2 and y == 1:
             if cells[7] == '_':
                 cells[7] = 'X'
-                return table(cells)
+                table(cells)
+                if '_' in cells:
+                    return wins1(cells)
+                else:
+                    return wins2(cells)
             else:
                 print("This cell is occupied! Choose another one!")
                 return game(cells)
         elif x == 2 and y == 2:
             if cells[4] == '_':
                 cells[4] = 'X'
-                return table(cells)
+                table(cells)
+                if '_' in cells:
+                    return wins1(cells)
+                else:
+                    return wins2(cells)
             else:
                 print("This cell is occupied! Choose another one!")
                 return game(cells)
         elif x == 2 and y == 3:
             if cells[1] == '_':
                 cells[1] = 'X'
-                return table(cells)
+                table(cells)
+                if '_' in cells:
+                    return wins1(cells)
+                else:
+                    return wins2(cells)
             else:
                 print("This cell is occupied! Choose another one!")
                 return game(cells)
         elif x == 3 and y == 1:
             if cells[8] == '_':
                 cells[8] = 'X'
-                return table(cells)
+                table(cells)
+                if '_' in cells:
+                    return wins1(cells)
+                else:
+                    return wins2(cells)
             else:
                 print("This cell is occupied! Choose another one!")
                 return game(cells)
         elif x == 3 and y == 2:
             if cells[5] == '_':
                 cells[5] = 'X'
-                return table(cells)
+                table(cells)
+                if '_' in cells:
+                    return wins1(cells)
+                else:
+                    return wins2(cells)
             else:
                 print("This cell is occupied! Choose another one!")
                 return game(cells)
         elif x == 3 and y == 3:
             if cells[2] == '_':
                 cells[2] = 'X'
-                return table(cells)
+                table(cells)
+                if '_' in cells:
+                    return wins1(cells)
+                else:
+                    return wins2(cells)
             else:
                 print("This cell is occupied! Choose another one!")
                 return game(cells)
-        return table(cells)
+    elif (x > 3 or x < 1) or (y > 3 or y < 1):
+        print("Coordinates should be from 1 to 3!")
+        return game(cells)
+
+def moveO(x, y):
+    if 1 <= x <= 3 and 1 <= y <= 3:
+        if x == 1 and y == 1:
+            if cells[6] == '_':
+                cells[6] = 'O'
+                table(cells)
+                if '_' in cells:
+                    return wins1(cells)
+                else:
+                    return wins2(cells)
+            else:
+                print("This cell is occupied! Choose another one!")
+                return game(cells)
+        elif x == 1 and y == 2:
+            if cells[3] == '_':
+                cells[3] = 'O'
+                table(cells)
+                if '_' in cells:
+                    return wins1(cells)
+                else:
+                    return wins2(cells)
+            else:
+                print("This cell is occupied! Choose another one!")
+                return game(cells)
+        elif x == 1 and y == 3:
+            if cells[0] == '_':
+                cells[0] = 'O'
+                table(cells)
+                if '_' in cells:
+                    return wins1(cells)
+                else:
+                    return wins2(cells)
+            else:
+                print("This cell is occupied! Choose another one!")
+                return game(cells)
+        elif x == 2 and y == 1:
+            if cells[7] == '_':
+                cells[7] = 'O'
+                table(cells)
+                if '_' in cells:
+                    return wins1(cells)
+                else:
+                    return wins2(cells)
+            else:
+                print("This cell is occupied! Choose another one!")
+                return game(cells)
+        elif x == 2 and y == 2:
+            if cells[4] == '_':
+                cells[4] = 'O'
+                table(cells)
+                if '_' in cells:
+                    return wins1(cells)
+                else:
+                    return wins2(cells)
+            else:
+                print("This cell is occupied! Choose another one!")
+                return game(cells)
+        elif x == 2 and y == 3:
+            if cells[1] == '_':
+                cells[1] = 'O'
+                table(cells)
+                if '_' in cells:
+                    return wins1(cells)
+                else:
+                    return wins2(cells)
+            else:
+                print("This cell is occupied! Choose another one!")
+                return game(cells)
+        elif x == 3 and y == 1:
+            if cells[8] == '_':
+                cells[8] = 'O'
+                table(cells)
+                if '_' in cells:
+                    return wins1(cells)
+                else:
+                    return wins2(cells)
+            else:
+                print("This cell is occupied! Choose another one!")
+                return game(cells)
+        elif x == 3 and y == 2:
+            if cells[5] == '_':
+                cells[5] = 'O'
+                table(cells)
+                if '_' in cells:
+                    return wins1(cells)
+                else:
+                    return wins2(cells)
+            else:
+                print("This cell is occupied! Choose another one!")
+                return game(cells)
+        elif x == 3 and y == 3:
+            if cells[2] == '_':
+                cells[2] = 'O'
+                table(cells)
+                if '_' in cells:
+                    return wins1(cells)
+                else:
+                    return wins2(cells)
+            else:
+                print("This cell is occupied! Choose another one!")
+                return game(cells)
     elif (x > 3 or x < 1) or (y > 3 or y < 1):
         print("Coordinates should be from 1 to 3!")
         return game(cells)
 
 
-table(cells=cells)
-game(cells=cells)
-
-'''def impossible(cells):
-    if (cells.count('O') != cells.count('X')) and (
-            cells.count('O') != cells.count('X') - 1 and cells.count('X') != cells.count('O') - 1):
-        return 'Impossible'
-    elif cells[0] == cells[1] == cells[2] and cells[3] == cells[4] == cells[5] or cells[0] == cells[1] == cells[2] and \
-            cells[6] == cells[7] == cells[8] or cells[3] == cells[4] == cells[5] and cells[6] == cells[7] == cells[8] or \
-            cells[0] == cells[3] == cells[6] and cells[1] == cells[4] == cells[7] or cells[0] == cells[3] == cells[
-        6] and cells[2] == cells[5] == cells[8] or cells[1] == cells[4] == cells[7] and cells[2] == cells[5] == cells[
-        8]:
-        return 'Impossible'
-
-
-def wins1(cells):
-    if cells[0] == cells[1] == cells[2]:
-        print(cells[0], 'wins')
-    elif cells[3] == cells[4] == cells[5]:
-        print(cells[3], 'wins')
-    elif cells[6] == cells[7] == cells[8]:
-        print(cells[6], 'wins')
-    elif cells[0] == cells[3] == cells[6]:
-        print(cells[0], 'wins')
-    elif cells[1] == cells[4] == cells[7]:
-        print(cells[1], 'wins')
-    elif cells[2] == cells[5] == cells[8]:
-        print(cells[2], 'wins')
-    elif cells[0] == cells[4] == cells[8]:
-        print(cells[0], 'wins')
-    elif cells[2] == cells[4] == cells[6]:
-        print(cells[2], 'wins')
-    else:
-        print('Game not finished')
-
-
-def wins2(cells):
-    if cells[0] == cells[1] == cells[2]:
-        print(cells[0], 'wins')
-    elif cells[3] == cells[4] == cells[5]:
-        print(cells[3], 'wins')
-    elif cells[6] == cells[7] == cells[8]:
-        print(cells[6], 'wins')
-    elif cells[0] == cells[3] == cells[6]:
-        print(cells[0], 'wins')
-    elif cells[1] == cells[4] == cells[7]:
-        print(cells[1], 'wins')
-    elif cells[2] == cells[5] == cells[8]:
-        print(cells[2], 'wins')
-    elif cells[0] == cells[4] == cells[8]:
-        print(cells[0], 'wins')
-    elif cells[2] == cells[4] == cells[6]:
-        print(cells[2], 'wins')
-    else:
-        print('Draw')'''
+table(cells)
+game(cells)
